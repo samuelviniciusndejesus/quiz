fef9abb16a5d222b6d5a680fff54267256b70574
samuelviniciusndejesus <samuelviniciusndejesus@users.noreply.github.com>
2019-05-21 13:39:31 -0300

    adcionei

---
+++ questions.js
@@ -0,0 +1,19 @@
+var questions = [
+
+    ["(Unesp 2014)  Em um plano horizontal encontram-se representadas uma circunferência e as cordas AC e BD. Nas condições apresentadas na figura, determine o valor de x.",
+        "x = 0 (não convém) ou x = 7", "x = 90", "x = 2,3", "x = 8","x = 18",
+        "img/ex1.png", "", "", "", "","",
+        "1"
+    ],
+    ["(Unicamp 2010)  Um artesão precisa recortar um retângulo de couro com   O retalho de couro disponível para a obtenção dessa tira é mostrado na figura a seguir. O retalho semicircular pode ser usado para a obtenção da tira? Justifique.",
+    "", "", "", "","",
+    "", "", "", "", "","",
+    ""
+    ],
+    ["",
+    "", "", "", "","",
+    "img/ex3.png", "", "", "", "","",
+    ""
+    ],
+
+]
